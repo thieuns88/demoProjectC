8f04c979ec3f1dcbd588fbc7accab434c372bd89
thieuns <thieuns88@gmail.com>
2020-11-24 15:34:17 +0700

    First upload

---
+++ Date.h
@@ -0,0 +1,8 @@
+//Date.h luu nguyen mau
+typedef struct
+{
+	int day,month,year;
+} Date;
+
+void inputDate(Date *d);
+void outputDate(Date d);
---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Date.h"
+
+/* Xu ly ngay thang */
+
+int main(int argc, char *argv[]) {
+	Date d;
+	inputDate(&d);
+	outputDate(d);
+	return 0;
+}
